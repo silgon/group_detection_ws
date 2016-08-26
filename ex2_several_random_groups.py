3c728c906acfa70a8e2b77915ee08fc2b6f282a3
silgon <silgon3200@gmail.com>
2016-08-26 18:22:19 -0400

    added baseline

--- ex2_several_random_groups.py
+++ ex2_several_random_groups.py
@@ -8,21 +8,34 @@ options = [ff.Cshape, ff.visAvis, ff.sideBySide, ff.Vshape]
 centers = [(-1.6, -1.6, 0), (1.6, -1.6, np.pi/2),
            (1.6, 1.6, np.pi/6), (-1.6, 1.6, 0), (0., 0.,  0.)]
 
-np.random.seed(3)
+np.random.seed(5)
 for id_sim in xrange(n_simulations):
     # random selection
     g = []
     cn = copy(centers) # copy of centers
     n_groups = np.random.randint(1, 4)
-
+    sc = []
     for _ in xrange(n_groups):
         f = options[np.random.randint(len(options))]
         c = cn.pop(np.random.randint(len(cn)))
+        sc.append(c)
         g.append(f(center=c))
+    gids = []
+    pid = 0
+    for ig in g:
+        gid = []
+        for _ in ig:
+          gid.append(pid)
+          pid += 1
+        gids.append(gid)
     gs=[item for sublist in g for item in sublist]
     results = solve(gs, plot=True)
     print("------------------")
-    print("For simulation {}:".format(id_sim))
+    print("Simulation {}:".format(id_sim+1))
+    print("Baseline")
+    print("Centers: {}".format(sc))
+    print("Group Members: {}".format(gids))
+    print("Deduced")
     print("Centers: {}".format(results[1]))
     print("Group Members: {}".format(results[0]))
 
