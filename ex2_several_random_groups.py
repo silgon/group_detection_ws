659b54522e75c834d10ea1fb1c13224293952f09
silgon <silgon3200@gmail.com>
2016-08-26 12:00:38 -0400

    bug: same number of centers and repeated centers corrected

--- ex2_several_random_groups.py
+++ ex2_several_random_groups.py
@@ -7,15 +7,17 @@ n_simulations = 6
 options = [ff.Cshape, ff.visAvis, ff.sideBySide, ff.Vshape]
 centers = [(-1.6, -1.6, 0), (1.6, -1.6, np.pi/2),
            (1.6, 1.6, np.pi/6), (-1.6, 1.6, 0), (0., 0.,  0.)]
-n_groups = np.random.randint(1, 4)
 
+np.random.seed(3)
 for id_sim in xrange(n_simulations):
     # random selection
     g = []
+    cn = copy(centers) # copy of centers
+    n_groups = np.random.randint(1, 4)
+
     for _ in xrange(n_groups):
-        cn = copy(centers) # copy of centers
         f = options[np.random.randint(len(options))]
-        c = cn.pop(np.random.randint(len(centers)))
+        c = cn.pop(np.random.randint(len(cn)))
         g.append(f(center=c))
     gs=[item for sublist in g for item in sublist]
     results = solve(gs, plot=True)
